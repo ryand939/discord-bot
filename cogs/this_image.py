a38bb236331a4ca7d010df0ffc04133ac1a8788c
ryand939 <60891382+ryand939@users.noreply.github.com>
2022-11-01 15:50:51 -0400

    added caption cmd to this_image

--- cogs/this_image.py
+++ cogs/this_image.py
@@ -34,6 +34,18 @@ class ThisImage(commands.Cog, description="preset image editor"):
         capImg = self.captionGenerator.multiline_caption(RGBImg, "THIS IS LEME")
         await ctx.channel.send(file=await util.PIL_img_to_file(ctx, capImg, ext)) 
 
+    @this.command(name="caption", description="Custom image caption.")
+    async def new(self, ctx, *, input=None):
+        if input is not None:
+            newInput = input.split(":", 1)                  # get top text and bottom text from input=toptext:bottomtext
+            img, ext = await util.get_last_img(ctx, 6)      # get the last image sent in chat and its file ext
+            if ext == -1: return 0                          
+            if len(newInput) == 1:
+                capImg = self.captionGenerator.multiline_caption(img, newInput[0].strip())
+            else:
+                capImg = self.captionGenerator.multiline_caption(img, newInput[0].strip(), newInput[1].strip())
+            await ctx.channel.send(file=await util.PIL_img_to_file(ctx, img, ext)) 
+
     @this.command(name="soyphone", description="Soyphone reaction.")
     async def soyphone(self, ctx):
         img, ext = await util.get_last_img(ctx, 6)
